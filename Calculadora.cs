2e05806ae1df03f1b2a4f2194c6da7aecd1bd477
olivadariojose <olivadariojose@gmail.com>
2025-07-14 07:28:17 -0600

    Initial commit

---
+++ Calculadora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CalculadoraConsola
+{
+    internal class Calculadora
+    {
+        public double Sumar(double a, double b) => a + b;
+        public double Restar(double a, double b) => a - b;
+        public double Multiplicar(double a, double b) => a * b;
+        public double Dividir(double a, double b)
+        {
+            if(b == 0)
+                throw new DivideByZeroException("No se puede dividir entre cero.");
+            return a / b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraConsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculadora = new Calculadora();
+
+            while (true)
+            {
+                MostrarMenu();
+
+                int opcion = LeerOpcionMenu();
+
+                if (opcion == 5)
+                {
+                    Console.WriteLine("Saliendo de la calculadora...");
+                    break;
+                }
+
+                double num1 = LeerNumero("Ingresa el primer numero: ");
+                double num2 = LeerNumero("Ingrese el segundo numero:");
+
+                try
+                {
+                    double resultado = 0;
+                    switch (opcion)
+                    {
+                        case 1:
+                            resultado = calculadora.Sumar(num1, num2);
+                            break;
+                        case 2:
+                            resultado = calculadora.Restar(num1, num2);
+                            break;
+                        case 3:
+                            resultado = calculadora.Multiplicar(num1, num2);
+                            break;
+                        case 4:
+                            resultado = calculadora.Dividir(num1, num2);
+                            break;
+
+                    }
+                    Console.WriteLine($"\n>>> Resultado: {resultado}\n");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error : {ex.Message}\n");
+                }
+
+                Console.WriteLine("Presione Cualquier tecla para continuar...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+
+            private static void MostrarMenu()
+            {
+                Console.WriteLine("=== Calculadora Sencilla ===");
+                Console.WriteLine("Selecciona una opcion: ");
+                Console.WriteLine("1. SUMAR");
+                Console.WriteLine("2. RESTAR");
+                Console.WriteLine("3. MULTIPLICAR");
+                Console.WriteLine("4. DIVIDIR");
+                Console.WriteLine("5. SALIR");
+            }
+
+            private static int LeerOpcionMenu()
+            {
+                int opcion;
+                while (true)
+                {
+                    Console.WriteLine("Opcion (1-5): ");
+                    string entrada = Console.ReadLine();
+                    if (int.TryParse(entrada, out opcion) && opcion >= 1 && opcion <= 5)
+                        return opcion;
+                    Console.WriteLine("Opcion invalida. Intente de nuevo");
+                }
+            }
+
+            private static double LeerNumero(string mensaje)
+            {
+                double numero;
+                while (true)
+                {
+                    Console.Write(mensaje);
+                    if (double.TryParse(Console.ReadLine(), out numero))
+                        return numero;
+                    Console.WriteLine("Entrada invalida. Intente de nuevo");
+                }
+            }
+  
+
+        }
+
+
+
+    }
+
