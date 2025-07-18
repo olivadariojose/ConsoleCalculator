453cbfb07f5959d39fa0669bf2b6a3e0037cc22e
olivadariojose <olivadariojose@gmail.com>
2025-07-17 19:39:56 -0600

    flag 17072025'

--- Calculator.cs
+++ Calculator.cs
@@ -2,16 +2,34 @@
 
 namespace ConsoleCalculator
 {
-    internal class Calculator
+    public class Calculator
     {
-        public double Add(double x, double y) => x + y;
-        public double Substract(double x, double y) => x - y;
-        public double Multiply(double x, double y) => x * y;
+        public double Add(double x, double y) => Checked(()=> x + y);
+        public double Substract(double x, double y) => Checked(() => x - y);
+        public double Multiply(double x, double y) => Checked(() => x * y);
         public double Divide(double x, double y)
         {
-            if(y == 0)
+            if(y == 0d || y == -0d)
                 throw new DivideByZeroException("No se puede dividir entre cero.");
-            return x / y;
+            return Checked(()=> x / y);
+        }
+
+        private static double Checked(Func<double> op)
+        {
+            double result;
+            try
+            {
+                result = op();
+            }
+            catch (OverflowException)
+            {
+                throw;
+            }
+
+            if (double.IsNaN(result) || double.IsInfinity(result))
+                throw new OverflowException("Resultado fuera del rango por double");
+
+            return result;
         }
     }
 }
